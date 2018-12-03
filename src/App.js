28d643311b6ad1e4a912c6e48426d8602ef3026a
Eddie Fong <fonged@oocl.com>
2018-12-03 14:46:54 +0800

    done button alert

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+  
+  alertFunction = () => {
+    alert("now you know how to handle onclick event");
+  }
+
   render() {
     return (
       <div className="App">
-        <button>
+        <button onClick = {this.alertFunction}>
           Hello world!
         </button>
         <span>
-          Number: 0
+          Number: 0 
         </span>
       </div>
     );
